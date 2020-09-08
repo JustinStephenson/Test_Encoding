cb092e07ddfca69dfb3995182a7728fd62d79d4d
Justin Stephenson <jstephenson@univeris.com>
2020-09-08 16:37:11 -0400

    adding

--- Script.sh
+++ Script.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-#find ./ -name "*.sql" -type f -print0 | xargs -0 iconv -f UTF-8 -t UTF-8 > /dev/null 
-var=$(git log --pretty=format: --name-only --since="2020-09-08 1:00:00" | sort | uniq)
+var=$(git log --pretty=format: --name-only -- '*.sql' --since="2020-09-08 1:00:00" | sort | uniq)
 for v in $var
 do
-	echo $v
-	iconv -f UTF-8 -t UTF-8 < $v > /dev/null 
+	if iconv -f UTF-8 -t UTF-8 < $v > /dev/null
+	then
+		:
+	else
+		echo $v
+	fi
 done
